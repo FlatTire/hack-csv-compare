dddcdfba8f0ea658265c2b06c36420f24caf9dae
Ben Tomasik <ben@tomasik.io>
2015-11-11 20:57:57 -0600

    WIP: Frame out feedanalyzer.FeedAnalyzer class Currently untested and _find_col_changes is broken

--- feedanalyzer.py
+++ feedanalyzer.py
@@ -134,40 +134,150 @@ class ColumnChange(DataChange):
         )
 
 
-def index_file(file_path, row_class, delimiter="\t", skip_rows=1):
-    """Reads a structured file and returns the data in a nice data structure
+class FeedAnalyzer(object):
+    DEFAULT_DELIMITER = "\t"
+    """Default delimiter to be used if none is specified explicitly"""
 
-    Args:
-        file_path (str): String path to an input file
-        row_class (cls): Class to 'typecast' data rows as
-        delimiter (str): Column delimiter within file
-        skip_rows (int): Number of rows to skip reading (ex. headers)
-    Return:
-        dict
-    """
-    data = {}
-    duplicates = []
-    with open(file_path, 'r') as input_file:
-        reader = csv.reader(input_file, delimiter=delimiter)
+    DEFAULT_SKIP_ROWS = 1
+    """Default number of rows to skip when reading files (ie. header rows)"""
 
-        for row in reader:
-            if skip_rows > 0:
-                skip_rows -= 1
-                logging.debug("Skipping row...")
+    @classmethod
+    def _find_col_changes(cls, data_left, data_right):
+        """Compares two dicts and outputs the changes in columns
+
+        Args:
+            data_left (list): List of left files rows
+            data_right (list): List of right files rows
+        Return:
+            list
+        """
+        column_changes = []
+        for key, value in data_right.items():
+            break
+            if key not in data_left:
                 continue
 
-            datarow = row_class(*row)
-            if datarow.hash in data:
-                # This is a duplicate
-                duplicates.append(datarow.hash)
+            if value != data_left[key]:
+                # Not equal, *some* differences
+                logging.debug("Column difference at '{k}'".format(k=key))
+                attr_list = data_left[key].find_different_columns(
+                    data_right[key])
 
-            data[datarow.hash] = datarow
+                for attr in attr_list:
+                    logging.warning("{k}: '{l}' -> '{r}'".format(
+                        k=attr,
+                        l=getattr(data_left[key], attr),
+                        r=getattr(data_right[key], attr)))
 
-    if len(duplicates):
-        # Duplicates detected, bail now
-        for duplicate_hash in duplicates:
-            logging.error("Duplicate: '{key}'".format(key=duplicate_hash))
+        return column_changes
 
-        raise DuplicateRowsError("Cannot continue with duplicates")
+    @classmethod
+    def _find_row_changes(cls, data_left, data_right):
+        """Compares two dicts and outputs the additions and deletions
 
-    return data
+        Args:
+            data_left (list): List of left files rows
+            data_right (list): List of right files rows
+        Return:
+            list
+        """
+        datachanges = []
+        for key, value in data_left.items():
+            if key not in data_right:
+                # Deleted
+                logging.error(
+                    "Deletion: '{k}'".format(
+                        k=key))
+                datachanges.append(Deletion(value))
+
+        for key, value in data_right.items():
+            if key not in data_left:
+                # Added
+                logging.error(
+                    "Addition: '{k}'".format(
+                        k=key))
+                datachanges.append(Addition(value))
+
+        return datachanges
+
+
+    @classmethod
+    def _index_file(cls, file_path, row_class, delimiter, skip_rows):
+        """Reads a structured file and returns the data in a nice data structure
+
+        Args:
+            file_path (str): String path to an input file
+            row_class (cls): Class to 'typecast' data rows as
+            delimiter (str): Column delimiter within file
+            skip_rows (int): Number of rows to skip reading (ex. headers)
+        Return:
+            dict
+        """
+        data = {}
+        duplicates = []
+        with open(file_path, 'r') as input_file:
+            reader = csv.reader(input_file, delimiter=delimiter)
+
+            for row in reader:
+                if skip_rows > 0:
+                    skip_rows -= 1
+                    logging.debug("Skipping row...")
+                    continue
+
+                datarow = row_class(*row)
+                if datarow.hash in data:
+                    # This is a duplicate
+                    duplicates.append(datarow.hash)
+
+                data[datarow.hash] = datarow
+
+        if len(duplicates):
+            # Duplicates detected, bail now
+            for duplicate_hash in duplicates:
+                logging.error("Duplicate: '{key}'".format(key=duplicate_hash))
+
+            raise DuplicateRowsError("Cannot continue with duplicates")
+
+        return data
+
+    def __init__(self, left_file, right_file, row_class, delimiter=None,
+                 skip_rows=-1):
+        """
+        Construct new analysis class for drilling into semi-structured files
+        to discover differences at the row and/or column level
+
+        :param left_file:
+        :type left_file: str
+        :param right_file:
+        :param row_class:
+        :param delimiter:
+        :param skip_rows:
+        :return:
+        """
+        self.left_file = left_file
+        self.right_file = right_file
+        self.row_class = row_class
+        self.delimiter = delimiter if delimiter else self.DEFAULT_DELIMITER
+        self.skip_rows = skip_rows if skip_rows > -1 else self.DEFAULT_SKIP_ROWS
+
+    def compare(self):
+        """Read in files, compare changes, output list of DataChange instances
+
+        Returns:
+            list(DataChange): All changes detected between files
+        """
+        # 1. Read in left file
+        data_left = self._index_file(self.left_file, self.row_class,
+                                     self.delimiter, self.skip_rows)
+        # 2. Read in right file
+        data_right = self._index_file(self.right_file, self.row_class,
+                                      self.delimiter, self.skip_rows)
+
+        # 3. Init a list for collating all changes detected
+        changes = []
+
+        # 4. Find row-level changes related to hash
+        changes += self._find_row_changes(data_left, data_right)
+
+        # 5. Find column-level changes related to individual column analysis
+        changes += self._find_col_changes(data_left, data_right)
