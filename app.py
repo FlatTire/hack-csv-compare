643fd53ac86f262500cbae05a72d318c3a63c7f7
Ben Tomasik <ben@tomasik.io>
2015-11-28 19:38:33 -0600

    Basic service template

---
+++ app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.4
+import os
+from flask import Flask, request, redirect, render_template, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+DEFAULT_SKIP_ROWS = 1
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET'])
+def upload_page():
+    """Shows the upload form"""
+    return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def compare_files():
+    """Compare two uploaded files"""
+    left = request.files['left']
+    right = request.files['right']
+
+    return """<pre>Trying...</pre>"""
+
+if __name__ == '__main__':
+    app.run()
